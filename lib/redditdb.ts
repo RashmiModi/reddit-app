653a2aef5e252c3735d65bf8425a389e1804a28c
RashmiModi <52698460+RashmiModi@users.noreply.github.com>
2025-05-19 13:52:21 +0530

    final commit

--- lib/redditdb.ts
+++ lib/redditdb.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/prisma/src/generated/client';
 
 declare global {
   // Prevent multiple instances of Prisma Client in development
