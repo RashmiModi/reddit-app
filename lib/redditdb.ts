00359a73b05b5f6f3af1b22d7c7fe19d1ab886fa
RashmiModi <52698460+RashmiModi@users.noreply.github.com>
2025-05-19 10:42:23 +0530

    final commit

--- lib/redditdb.ts
+++ lib/redditdb.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-// Extend the global type
 declare global {
+  // Prevent multiple instances of Prisma Client in development
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-// Create prisma client or reuse existing one
 const prisma = global.prisma || new PrismaClient({
   log: ['error'],
 });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
 
 export default prisma;
